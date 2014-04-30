4acd6807cf4740c10e3e9da002b7741efbf58a7e
Eddie Arevalo <eddiesarevalo@gmail.com>
2014-04-29 17:47:31 -0700

    Added Values for ForcastList
    to do:
    need to show the values and image

--- WeatherApp/gen/com/example/weatherApp/R.java
+++ WeatherApp/gen/com/example/weatherApp/R.java
@@ -32,14 +32,19 @@ public final class R {
     }
     public static final class id {
         public static final int TableRow01=0x7f090002;
-        public static final int action_settings=0x7f090008;
-        public static final int city_name=0x7f090006;
-        public static final int forcast_list=0x7f090007;
-        public static final int humidityView=0x7f090003;
+        public static final int action_settings=0x7f09000d;
+        public static final int city_name=0x7f09000b;
+        public static final int date_view=0x7f090003;
+        public static final int forcast_list=0x7f09000c;
         public static final int imageView1=0x7f090001;
+        public static final int image_view=0x7f09000a;
         public static final int main_layout=0x7f090000;
+        public static final int temp_day_view=0x7f090007;
+        public static final int temp_eve_view=0x7f090008;
         public static final int temp_max_view=0x7f090005;
         public static final int temp_min_view=0x7f090004;
+        public static final int temp_morn_view=0x7f090006;
+        public static final int temp_night_view=0x7f090009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- WeatherApp/src/com/example/weatherApp/ForcastListAdapter.java
+++ WeatherApp/src/com/example/weatherApp/ForcastListAdapter.java
@@ -1,5 +1,7 @@
 package com.example.weatherApp;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import com.example.weatherApp.forcast.Forcast;
@@ -31,22 +33,49 @@ public class ForcastListAdapter extends ArrayAdapter<Forcast>{
 		
 		Forcast p = getItem(position);
 		if(p != null){
-			TextView humidityView = (TextView) v.findViewById(R.id.humidityView);
+			TextView dateView = (TextView) v.findViewById(R.id.date_view);
 			TextView tempMinView = (TextView) v.findViewById(R.id.temp_min_view);
-			TextView tempMaxView = (TextView) v.findViewById(R.id.temp_min_view);
+			TextView tempMaxView = (TextView) v.findViewById(R.id.temp_max_view);
+			TextView tempMornView = (TextView) v.findViewById(R.id.temp_morn_view);
+			TextView tempDayView = (TextView) v.findViewById(R.id.temp_day_view);
+			TextView tempEveView = (TextView) v.findViewById(R.id.temp_eve_view);
+			TextView tempNightView = (TextView) v.findViewById(R.id.temp_night_view);
 			
-		
-			if (humidityView != null) {
-				humidityView.setText("humidity: " + p.humidity);
+			String dateAsText = new SimpleDateFormat("dd/MM/yyyy").format(new Date(p.dt * 1000L));
+			double tempMin = convertKelvinToFahrenheit(p.temp.min);
+			double tempMax = convertKelvinToFahrenheit(p.temp.max);
+			double tempMorn = convertKelvinToFahrenheit(p.temp.morn);
+			double tempDay = convertKelvinToFahrenheit(p.temp.day);
+			double tempEve = convertKelvinToFahrenheit(p.temp.eve);
+			double tempNight = convertKelvinToFahrenheit(p.temp.night);
+			
+			if (dateView != null) {
+				dateView.setText(dateAsText);
 	        }
 			if (tempMinView != null) {
-				tempMinView.setText("min: " + p.temp.min);
+				tempMinView.setText("min: " + tempMin);
 	        }
-	        if (tempMaxView != null) {
-	        	tempMaxView.setText("max: " + p.temp.max);
+			if (tempMaxView != null) {
+	        	tempMaxView.setText("max: " + tempMax);
 	        }
+	        if (tempMornView != null) {
+	        	tempMornView.setText("morn: " + tempMorn);
+	        }
+	        if (tempDayView != null) {
+	        	tempDayView.setText("day: " + tempDay);
+	        }
+	        if (tempEveView != null) {
+	        	tempEveView.setText("eve: " + tempEve);
+	        }
+	        if (tempNightView != null) {
+	        	tempNightView.setText("night: " + tempNight);
+	        }
+	        
         }
 		
         return v;
+	}
+	private double convertKelvinToFahrenheit(double kelvinTemp){
+		return 9/(5 * (kelvinTemp - 273) + 32);
 	}
 }
